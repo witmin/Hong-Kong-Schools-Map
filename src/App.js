b303a20da1618f5bce2b874c12fff947dab9ef9f
Millie Lin <linxinmin@gmail.com>
2019-12-15 21:51:10 +0800

    Added China Geo Json data.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import './index.css';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_PERSONAL_ACCESS_TOKEN;
 
+let hoveredStateId = null;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             lng: 103,
             lat: 34,
-            zoom: 4
+            zoom: 4,
         }
     }
 
@@ -21,15 +23,82 @@ class App extends React.Component {
             container: this.mapContainer,
             style: 'mapbox://styles/witmin/ck46v5ez92o7r1cmml0s0svky',
             center: [this.state.lng, this.state.lat],
-            zoom: this.state.zoom
+            zoom: this.state.zoom,
         });
 
-        map.on('move', ()=> {
+        map.on('move', () => {
             this.setState({
                 lng: map.getCenter().lng.toFixed(4),
                 lat: map.getCenter().lat.toFixed(4),
                 zoom: map.getZoom().toFixed(2)
-            })
+            });
+        });
+
+        map.on('load', function () {
+            map.addSource('states', {
+                'type': 'geojson',
+                'data':
+                    'https://raw.githubusercontent.com/waylau/svg-china-map/master/china-map/china.geo.json'
+            });
+
+// The feature-state dependent fill-opacity expression will render the hover effect
+// when a feature's hover state is set to true.
+            map.addLayer({
+                'id': 'state-fills',
+                'type': 'fill',
+                'source': 'states',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#627BC1',
+                    'fill-opacity': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        1,
+                        0.5
+                    ]
+                }
+            });
+
+            map.addLayer({
+                'id': 'state-borders',
+                'type': 'line',
+                'source': 'states',
+                'layout': {},
+                'paint': {
+                    'line-color': '#627BC1',
+                    'line-width': 2
+                }
+            });
+
+// When the user moves their mouse over the state-fill layer, we'll update the
+// feature state for the feature under the mouse.
+            map.on('mousemove', 'state-fills', function (e) {
+                if (e.features.length > 0) {
+                    if (hoveredStateId) {
+                        map.setFeatureState(
+                            {source: 'states', id: hoveredStateId},
+                            {hover: false}
+                        );
+                    }
+                    hoveredStateId = e.features[0].id;
+                    map.setFeatureState(
+                        {source: 'states', id: hoveredStateId},
+                        {hover: true}
+                    );
+                }
+            });
+
+// When the mouse leaves the state-fill layer, update the feature state of the
+// previously hovered feature.
+            map.on('mouseleave', 'state-fills', function () {
+                if (hoveredStateId) {
+                    map.setFeatureState(
+                        {source: 'states', id: hoveredStateId},
+                        {hover: false}
+                    );
+                }
+                hoveredStateId = null;
+            });
         });
 
     }
